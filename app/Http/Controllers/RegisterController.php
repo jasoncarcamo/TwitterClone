f25705dfab2f9c9fce11129ddfb65df8423db055
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-03-21 23:28:03 -0400

    Quick commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 
 use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
