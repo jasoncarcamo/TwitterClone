ac7e11bf9808c048c615cde2d65a1be30e251e7e
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-03-23 23:45:58 -0400

    Added tweets table schema

---
+++ database/migrations/2020_03_24_034149_create_tweets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->string('tweet_text');
+            $table->string('entities');
+            $table->timestamp('create_at');
+            $table->string('geo_lat');
+            $table->string('geo_long');
+            $table->string('screen_name');
+            $table->string('name');
+            $table->string('profile_image_url');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
