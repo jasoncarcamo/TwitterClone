618133d9942b1587125ec522e007e6d9104e2a8d
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-03-24 20:17:57 -0400

    Added cors middleware

--- routes/api.php
+++ routes/api.php
@@ -14,51 +14,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-//Register route
-Route::group(['prefix' => 'register'], function () {
-    Route::post('/', 'RegisterController@register')->name('register');
-});
-
-//Login route
-Route::group(['prefix' => 'login'], function () {
-    Route::post('/', 'LoginController@login')->name('login')->middleware("cors");
-});
-
-//Users routes
 Route::group([
-    'prefix' => 'user',
-], function () {
-    Route::get('/', 'UserController@index')->name('user');
-    Route::post('/', 'UserController@create')->name('createsusers');
-});
-
-//Tweets routes
-Route::group([
-    'prefix' => 'tweets',
-    'middleware' => 'jwtAuth'
-], function () {
-    Route::get('/', 'TweetsController@getUsersTweets')->name('usertweets');
-    Route::post('/', 'TweetsController@createTweet')->name('createtweet');
-    Route::patch('/', 'TweetsController@updateTweet')->name('updatetweet');
-    Route::delete('/', 'TweetsController@deleteTweet')->name('deletetweet');
-});
-
-//Follow routes
-Route::group([
-    'prefix' => '/',
-    'middleware' => 'jwtAuth'
+    'middleware' => 'cors'
 ], function () {
 
-    //Users followers
-    Route::get('/followers', 'FollowController@getFollowers')->name('followers');
+    //Register route
+    Route::group(['prefix' => 'register'], function () {
+        Route::post('/', 'RegisterController@register')->name('register');
+    });
+
+    //Login route
+    Route::group(['prefix' => 'login'], function () {
+        Route::post('/', 'LoginController@login')->name('login')->middleware("cors");
+    });
+
+    //Users routes
+    Route::group([
+        'prefix' => 'user',
+    ], function () {
+        Route::get('/', 'UserController@index')->name('user');
+        Route::post('/', 'UserController@create')->name('createsusers');
+    });
+
+    //Tweets routes
+    Route::group([
+        'prefix' => 'tweets',
+        'middleware' => 'jwtAuth'
+    ], function () {
+        Route::get('/', 'TweetsController@getUsersTweets')->name('usertweets');
+        Route::post('/', 'TweetsController@createTweet')->name('createtweet');
+        Route::patch('/', 'TweetsController@updateTweet')->name('updatetweet');
+        Route::delete('/', 'TweetsController@deleteTweet')->name('deletetweet');
+    });
+
+    //Follow routes
+    Route::group([
+        'prefix' => '/',
+        'middleware' => 'jwtAuth'
+    ], function () {
+
+        //Users followers
+        Route::get('/followers', 'FollowController@getFollowers')->name('followers');
 
 
-    //Users following
-    Route::get('/following', 'FollowController@getFollowing')->name('follwing');
+        //Users following
+        Route::get('/following', 'FollowController@getFollowing')->name('follwing');
 
-    //Follow route
-    Route::post('/follow', 'FollowController@followUser')->name('followuser');
-    Route::delete('/follow', 'FollowController@unfollowUser')->name('unfollowuser');
+        //Follow route
+        Route::post('/follow', 'FollowController@followUser')->name('followuser');
+        Route::delete('/follow', 'FollowController@unfollowUser')->name('unfollowuser');
+    });
 });
